45039d6fbe77cc7d813300c8bfbd0400da8d071d
AhmedDarwish74 <ahmed.darwish774@gmail.com>
2024-09-15 22:33:13 +0300

    push new simple_interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+principal = 1000 # principal amount in dollar$
+rate = 0.05 #annual  interst rate 5%
+time = 3 #time in year
+
+#calculate simple interest 
+interest = principal * rate * time
+
+print("The simple interest is", interest)
